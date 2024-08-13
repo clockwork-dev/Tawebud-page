61419077e81eaaf83d8bef4a126567e2e4a5db0f
Rodion Prysenko <masynykot5@gmail.com>
2024-08-13 19:35:23 +0200

    fix display mod

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -9,7 +9,8 @@ export const Header = (props) => {
         <div className="overlay">
           <div className="container">
             <div className="row">
-              <div className="col-md-8 col-md-offset-2 intro-text">
+              <div className="col-md-8 col-md-offset-2 intro-text"> 
+                {/* Здесь мы убрали лишний div  */}
                 <h1 className="fade-in">
                   {t('Header.title')}
                 </h1>
